c8d1e284560edf2119bd4be856fad855518494c7
Cameron Priestley <cameronpriestley@gmail.com>
2016-09-04 15:32:39 -0400

    update with node and express

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var morgan = require('morgan');
+
+var bodyParser = require('body-parser');
+
+
+
+var app = express();
+
+
+
+
+
+
+app.listen(3000);
+
+
+console.log("App is working");
