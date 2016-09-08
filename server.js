87bb04e4aced967e5c7eb7ce8fc620e614249982
Cameron Priestley <cameronpriestley@gmail.com>
2016-09-07 21:14:24 -0400

    update express server

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 var express = require('express');
+var app = express();
 
+var router = express.Router();
+var path = require('path');
 var morgan = require('morgan');
-
 var bodyParser = require('body-parser');
 
 
 
-var app = express();
+app.get('/', function(req, res){
+
+    res.sendFile(path.join(__dirname + '/index.html'));
+
+});
+// router.get('/', function(req, res, next){
+//
+//
+//     // res.sendFile(path.join(__dirname, '../', 'views','index.html'));
+//     res.sendFile('index.html');
+//
+//
+//
+// });
+//
+// module.exports = router;
 
 
 
+// app.listen(3000);
+var server = app.listen(3000, function () {
 
+var host = server.address().address;
+var port = server.address().port;
 
+});
 
-app.listen(3000);
 
 
 console.log("App is working");
