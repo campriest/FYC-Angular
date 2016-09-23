de900d085740ec855d91c2b2251c88581c0dc3e2
Cameron Priestley <cameronpriestley@gmail.com>
2016-09-23 10:31:12 -0400

    routing update

--- server.js
+++ server.js
@@ -6,30 +6,60 @@ var path = require('path');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
+var pg = require('pg');
 
+var connectString= "postgres://fyc_master:fycmaster@localhost/fycdata";
+var pgClient = new pg.Client(connectstring);
+pgClient.connect();
+var query = pgCient.query();
 
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
 
+    pg.connect(connect, function(err, client, done){
+        if(err){
+
+            return console.error("error fetching", err);
+        }
+
+
+
+    })
+
     res.sendFile('/index.html');
 
 });
-// router.get('/', function(req, res, next){
-//
-//
-//     // res.sendFile(path.join(__dirname, '../', 'views','index.html'));
-//     res.sendFile('index.html');
-//
-//
-//
-// });
-//
-// module.exports = router;
+
+var signUp = express.Router();
+
+    signUp.get('', function(req, res){
 
 
+    });
 
-// app.listen(3000);
+
+    signUp.post('', function(req, res){
+
+    });
+
+var albumLoad = express.Routher();
+
+    albumLoad.get('', function(req, res){
+
+
+    });
+
+    albumLoad.post('', function(req, res){
+
+    });
+
+    albumLoad.patch('', function(req, res){
+
+
+    });
+
+
 var server = app.listen(3000, function () {
 
 var host = server.address().address;
